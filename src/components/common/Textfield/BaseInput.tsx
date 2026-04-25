3429bfd3827e1e3cb0719c68aec4b244ec7724ec
봄 <saebom@Saebomui-MacBookPro.local>
2026-04-26 04:46:41 +0900

    feat: BaseInput에 placeholder props 추가

--- src/components/common/Textfield/BaseInput.tsx
+++ src/components/common/Textfield/BaseInput.tsx
@@ -4,11 +4,15 @@ import { cn } from '@/lib';
 type BaseInputProps = React.ComponentProps<'input'>;
 
 export const BaseInput = React.forwardRef<HTMLInputElement, BaseInputProps>(
-  ({ className, ...rest }, ref) => {
+  ({ className, placeholder, ...rest }, ref) => {
     return (
       <input
         ref={ref}
-        className={cn('w-full min-w-0 outline-none disabled:cursor-not-allowed', className)}
+        placeholder={placeholder}
+        className={cn(
+          'w-full min-w-0 outline-none disabled:cursor-not-allowed text-text-default',
+          className,
+        )}
         {...rest}
       />
     );
