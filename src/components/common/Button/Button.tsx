7943c8fbd9d6fc63ab05579688dc3d2f190b8993
mlnls <tmdals140@naver.com>
2026-04-25 19:56:35 +0900

    refactor: button props 전달 방식 변경

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,17 +1,12 @@
 import { type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 import { cn } from '@/lib';
-import { ButtonSize, ButtonVariant, buttonVariants } from './buttonVariants';
+import { buttonVariants } from './buttonVariants';
 
-type Props = React.ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    className?: string;
-    size?: ButtonSize;
-    variant?: ButtonVariant;
-  };
+type Props = React.ComponentProps<'button'> & VariantProps<typeof buttonVariants>;
 
 export const Button = (props: Props) => {
-  const { variant, size, className } = props;
+  const { variant, size, className, ...rest } = props;
 
-  return <button className={cn(buttonVariants({ variant, size, className }))} {...props} />;
+  return <button className={cn(buttonVariants({ variant, size, className }))} {...rest} />;
 };
