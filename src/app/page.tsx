2dcbb9eeb866984921a24947e3df2355d9b9a8d9
mlnls <tmdals140@naver.com>
2026-04-25 18:27:25 +0900

    refactor: type import 방식 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from "next";
-import { MainContainer } from "@/components";
+import { type Metadata } from 'next';
+import { MainContainer } from '@/components';
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "Readum",
-    description: "Readum:사유하는 독서가인 당신을 위해",
+    title: 'Readum',
+    description: 'Readum:사유하는 독서가인 당신을 위해',
   };
 }
 
