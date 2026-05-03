ec340c0787adba695c3e3abc134d488e4a9d6cac
Saebom <spring180@naver.com>
2026-05-03 16:44:33 +0900

    refactor: 상수 타입 구조 개선

--- src/constants/textfieldChat.tsx
+++ src/constants/textfieldChat.tsx
@@ -13,8 +13,8 @@ export const CHAT_BG_VARIANT = {
 
 export type chatBgVariant = (typeof CHAT_BG_VARIANT)[keyof typeof CHAT_BG_VARIANT];
 
-export const CHAT_PLACEHOLDER: Record<chatStatus, string> = {
+export const CHAT_PLACEHOLDER = {
   [CHAT_STATUS.DEFAULT]: '오늘은 어떤 얘기를 해볼까요?',
   [CHAT_STATUS.DISABLED]: '연결을 확인해주세요',
   [CHAT_STATUS.ERROR]: '잘못된 입력',
-};
+} as const satisfies Record<chatStatus, string>;
