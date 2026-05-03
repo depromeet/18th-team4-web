e2dd3ec4ee2103a4dc68d31573dd87e4ae7fc9f6
poodlepoodle <chammal97@naver.com>
2026-05-03 10:45:25 +0900

    refactor: px에서 rem 단위로 수정

--- src/components/common/Tooltip/tooltipVariants.ts
+++ src/components/common/Tooltip/tooltipVariants.ts
@@ -4,16 +4,16 @@ export type TooltipArrowSide = 'top' | 'bottom';
 export type TooltipArrowAlignment = 'left' | 'middle' | 'right';
 
 export const tooltipVariants = cva(
-  'body2-bold relative inline-block whitespace-nowrap rounded-[10px] bg-gray-800 px-3 py-[10px] text-center text-gray-white tracking-[-0.03em]',
+  'body2-bold relative inline-block whitespace-nowrap rounded-[0.625rem] bg-gray-800 px-3 py-[0.63rem] text-center text-gray-white tracking-[-0.03em] select-none',
 );
 
 export const tooltipArrowVariants = cva(
-  'pointer-events-none absolute h-[8px] w-[20px] text-gray-800',
+  'pointer-events-none absolute h-[0.5rem] w-[1.25rem] text-gray-800',
   {
     variants: {
       arrowSide: {
-        top: '-top-[8px]',
-        bottom: '-bottom-[8px] rotate-180',
+        top: '-top-[0.5rem]',
+        bottom: '-bottom-[0.5rem] rotate-180',
       },
       arrowAlignment: {
         left: 'left-[1.31rem]',
