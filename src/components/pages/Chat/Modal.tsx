5a33b3ab7619be37d03fdedb30e65360dbda3073
봄 <saebom@Saebomui-MacBookPro.local>
2026-05-04 18:04:08 +0900

    feat: 모달 컴포넌트 스토리북 생성

--- src/components/pages/Chat/Modal.tsx
+++ src/components/pages/Chat/Modal.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import { createPortal } from 'react-dom';
 import { Summary } from '@/assets';
 import { Button, BUTTON_VARIANT } from '@/components';
-import { useIsMounted } from '@/hooks';
 
 type Props = {
   isOpen: boolean;
@@ -13,9 +12,7 @@ type Props = {
 };
 
 export const Modal = ({ isOpen, onCancel, onConfirm }: Props) => {
-  const mounted = useIsMounted();
-
-  if (!mounted || !isOpen) return null;
+  if (!isOpen) return null;
 
   return createPortal(
     <div
@@ -23,10 +20,12 @@ export const Modal = ({ isOpen, onCancel, onConfirm }: Props) => {
       onClick={onCancel}
     >
       <div
-        className="bg-text-white flex flex-col gap-[3rem] rounded-[20px] items-center p-[2.4rem] pt-[2.8rem] min-w-[330px]"
+        role="dialog"
+        aria-modal="true"
+        className="bg-text-white flex min-w-[330px] flex-col items-center gap-[3rem] rounded-[20px] p-[2.4rem] pt-[2.8rem]"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="flex flex-col gap-[0.2rem] items-center text-center">
+        <div className="flex flex-col items-center gap-[0.2rem] text-center">
           <Image src={Summary} alt="요약 중 로고" width={48} height={18} />
           <h3 className="text-text-default headline2-bold mt-[1.4rem]">대화를 마무리할까요?</h3>
           <p className="body2-medium text-text-description">
@@ -34,7 +33,7 @@ export const Modal = ({ isOpen, onCancel, onConfirm }: Props) => {
           </p>
         </div>
 
-        <div className="flex gap-[1rem] w-full">
+        <div className="flex w-full gap-[1rem]">
           <Button
             variant={BUTTON_VARIANT.LIGHTGRAY}
             size="lg"
