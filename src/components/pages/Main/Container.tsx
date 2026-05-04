28789fa7ab260a0b4a686b20f7a63d2c0e70cbe6
Jihaeee <elinkang02@gmail.com>
2026-05-05 02:50:23 +0900

    chore : 메인 페이지에 테스트용ChatCard 적용

--- src/components/pages/Main/Container.tsx
+++ src/components/pages/Main/Container.tsx
@@ -1,4 +1,4 @@
-import { Header, HEADER_VARIANT, LinkButton } from '@/components';
+import { ChatCard, CHAT_CARD_COLOR, CHAT_CARD_STATUS, Header, HEADER_VARIANT, LinkButton } from '@/components';
 import { PATH_NAME } from '@/constants';
 import { MainBody } from './Body';
 
@@ -7,6 +7,25 @@ export default function MainContainer() {
     <>
       <Header variant={HEADER_VARIANT.HOME} />
       <MainBody />
+
+      <div className="flex flex-col gap-[1.2rem] px-[2rem] py-[2.4rem]">
+        <ChatCard
+          color={CHAT_CARD_COLOR.TEAL}
+          date="25.10.10"
+          summary="대화한 내용 간단 요약 어쩌구 저쩌..."
+          bookmarked
+        />
+        <ChatCard
+          color={CHAT_CARD_COLOR.YELLOW}
+          status={CHAT_CARD_STATUS.LOADING}
+          date="25.10.10"
+        />
+        <ChatCard
+          color={CHAT_CARD_COLOR.SKY}
+          status={CHAT_CARD_STATUS.ERROR}
+        />
+        
+      </div>
 
       <section className="flex justify-center py-[2.4rem]">
         <LinkButton href={PATH_NAME.register.list()} size="lg" variant="black">
