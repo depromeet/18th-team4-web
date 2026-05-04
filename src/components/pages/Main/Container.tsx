2c3f238dbaae1d4935e7c6c79768ff329c04b343
Jihaeee <elinkang02@gmail.com>
2026-05-05 03:16:04 +0900

    feat: 등록하기, 둘러보기 버튼 추가

--- src/components/pages/Main/Container.tsx
+++ src/components/pages/Main/Container.tsx
@@ -1,37 +1,22 @@
-import { ChatCard, CHAT_CARD_COLOR, CHAT_CARD_STATUS, Header, HEADER_VARIANT, LinkButton } from '@/components';
+import { Header, HEADER_VARIANT, LinkButton } from '@/components';
 import { PATH_NAME } from '@/constants';
 import { MainBody } from './Body';
 
-export default function MainContainer() {
+const MainContainer = () => {
   return (
-    <>
+    <div className="flex min-h-dvh flex-col">
       <Header variant={HEADER_VARIANT.HOME} />
       <MainBody />
-
-      <div className="flex flex-col gap-[1.2rem] px-[2rem] py-[2.4rem]">
-        <ChatCard
-          color={CHAT_CARD_COLOR.TEAL}
-          date="25.10.10"
-          summary="대화한 내용 간단 요약 어쩌구 저쩌..."
-          bookmarked
-        />
-        <ChatCard
-          color={CHAT_CARD_COLOR.YELLOW}
-          status={CHAT_CARD_STATUS.LOADING}
-          date="25.10.10"
-        />
-        <ChatCard
-          color={CHAT_CARD_COLOR.SKY}
-          status={CHAT_CARD_STATUS.ERROR}
-        />
-        
-      </div>
-
-      <section className="flex justify-center py-[2.4rem]">
-        <LinkButton href={PATH_NAME.register.list()} size="lg" variant="black">
-          시작하기
+      <div className="flex flex-col gap-[1.6rem] px-[2.4rem] pb-[2.4rem]">
+        <LinkButton href={PATH_NAME.register.list()} size="lg" variant="black" className="rounded-[1.6rem]">
+          책 등록하기
         </LinkButton>
-      </section>
-    </>
+        <button type="button" className="body1-bold text-center tracking-[-0.048rem] text-text-caption">
+          둘러보기
+        </button>
+      </div>
+    </div>
   );
-}
+};
+
+export default MainContainer;
