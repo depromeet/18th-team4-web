f64f52bb9e6af48702c8281747f5977ac476700d
봄 <saebom@Saebomui-MacBookPro.local>
2026-05-04 18:46:24 +0900

    feat: Chat 페이지 메타데이터 설정

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,2 +1,3 @@
+export { default as ChatContainer } from './Chat/Container';
 export { default as MainContainer } from './Main/Container';
 export { default as RegisterContainer } from './Register/Container';
