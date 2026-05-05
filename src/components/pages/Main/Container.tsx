2f876d11eb8b7706d855963f9085ce6a37d330ed
Jihaeee <elinkang02@gmail.com>
2026-05-05 10:56:53 +0900

    feat: 공통헤더 삭제 및 책 등록 여부에 따른 분기

--- src/components/pages/Main/Container.tsx
+++ src/components/pages/Main/Container.tsx
@@ -1,20 +1,33 @@
 import { Header, HEADER_VARIANT, LinkButton } from '@/components';
 import { PATH_NAME } from '@/constants';
+import { BookBottomBar } from './BookBottomBar';
 import { MainBody } from './Body';
+import { EmptyRecordsBody } from './EmptyRecordsBody';
+
+const hasBook = false; 
 
 const MainContainer = () => {
+  if (!hasBook) {
+    return (
+      <div className="flex min-h-dvh flex-col">
+        <MainBody />
+        <div className="flex flex-col gap-[1.6rem] px-[2.4rem] pb-[2.4rem]">
+          <LinkButton href={PATH_NAME.register.list()} size="lg" variant="black" className="rounded-[1.6rem]">
+            책 등록하기
+          </LinkButton>
+          <button type="button" className="body1-bold text-center tracking-[-0.048rem] text-text-caption">
+            둘러보기
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex min-h-dvh flex-col">
       <Header variant={HEADER_VARIANT.HOME} />
-      <MainBody />
-      <div className="flex flex-col gap-[1.6rem] px-[2.4rem] pb-[2.4rem]">
-        <LinkButton href={PATH_NAME.register.list()} size="lg" variant="black" className="rounded-[1.6rem]">
-          책 등록하기
-        </LinkButton>
-        <button type="button" className="body1-bold text-center tracking-[-0.048rem] text-text-caption">
-          둘러보기
-        </button>
-      </div>
+      <EmptyRecordsBody />
+      <BookBottomBar bookTitle="해리 포터와 마법사의 돌 1" />
     </div>
   );
 };
