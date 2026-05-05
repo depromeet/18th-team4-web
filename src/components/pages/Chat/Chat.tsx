048b1dcb6351423a19fb804b76dc40945b932314
mlnls <tmdals140@naver.com>
2026-05-06 04:09:13 +0900

    feat: AI 채팅 아래에 로고 추가

--- src/components/pages/Chat/Chat.tsx
+++ src/components/pages/Chat/Chat.tsx
@@ -1,4 +1,5 @@
 import { VariantProps } from 'class-variance-authority';
+import { ColorSymbolIcon } from '@/components';
 import { CHAT_USER, ChatUser } from '@/constants';
 import { cn } from '@/lib';
 import { containerVariants } from './chatVariants';
@@ -14,6 +15,7 @@ export const Chat = (props: Props) => {
   return (
     <div className={cn(containerVariants({ user }))}>
       <p className="body2-semibold whitespace-pre-wrap break-words text-text-default">{message}</p>
+      {user === 'ai' ? <ColorSymbolIcon className="mt-[1.2rem]" /> : null}
     </div>
   );
 };
