ee4e9c9da08171955bd8c11880e6ba3455684191
Jihaeee <elinkang02@gmail.com>
2026-05-08 02:06:13 +0900

    feat: user-books.server.ts 추가해서 서버에서 데이터 가져오도록 변경

--- src/lib/api/services/user-books/user-books.client.ts
+++ src/lib/api/services/user-books/user-books.client.ts
@@ -1,16 +1,6 @@
 import { ENDPOINTS } from '@/constants';
 import { publicHttp } from '@/lib';
-import {
-  type AddUserBookRequest,
-  type AddUserBookResponse,
-  type UserBookListResponse,
-} from './user-books.type';
-
-export const getUserBooks = async () => {
-  const response = await publicHttp.get<UserBookListResponse>(ENDPOINTS.USER_BOOKS.list());
-
-  return response.data;
-};
+import { type AddUserBookRequest, type AddUserBookResponse } from './user-books.type';
 
 export const addUserBook = async (body: AddUserBookRequest) => {
   const response = await publicHttp.post<AddUserBookRequest, AddUserBookResponse>(
--- src/lib/api/services/user-books/user-books.service.ts
+++ src/lib/api/services/user-books/user-books.service.ts
@@ -1,15 +1,7 @@
 'use client';
 
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { QUERY_KEY } from '@/constants';
-import { addUserBook, getUserBooks } from './user-books.client';
-
-export const useGetUserBooks = () => {
-  return useQuery({
-    queryKey: QUERY_KEY.userBooks.list(),
-    queryFn: getUserBooks,
-  });
-};
+import { useMutation } from '@tanstack/react-query';
+import { addUserBook } from './user-books.client';
 
 export const useAddUserBook = () => {
   return useMutation({
