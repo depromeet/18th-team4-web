adc9adcf97c0d85bb8749e8dc670118f38f27459
mlnls <tmdals140@naver.com>
2026-05-08 03:22:27 +0900

    feat: gradient assets 추가

--- src/assets/common/webps/index.ts
+++ src/assets/common/webps/index.ts
@@ -1,4 +1,5 @@
 export { default as Shelve } from './empty-shelve.webp';
+export * from './gradient';
 export { default as HomeLogo } from './home-logo.webp';
 export { default as Logo } from './logo.webp';
 export { default as Summary } from './summary.webp';
