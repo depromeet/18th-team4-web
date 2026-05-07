6eb93a91d6f731f5d96da73752f083fd39c513bc
Saebom <spring180@naver.com>
2026-05-08 01:33:05 +0900

    feat: 답변 바로 반영되도록 적용

--- src/components/pages/Chat/Chat.tsx
+++ src/components/pages/Chat/Chat.tsx
@@ -7,14 +7,18 @@ import { containerVariants } from './chatVariants';
 type Props = VariantProps<typeof containerVariants> & {
   user?: ChatUser;
   message: string;
+  isStreaming?: boolean;
 };
 
 export const Chat = (props: Props) => {
-  const { user = CHAT_USER.ME, message } = props;
+  const { user = CHAT_USER.ME, message, isStreaming = false } = props;
 
   return (
     <div className={cn(containerVariants({ user }))}>
-      <p className="body2-semibold whitespace-pre-wrap break-words text-text-default">{message}</p>
+      <p className="body2-semibold whitespace-pre-wrap break-words text-text-default">
+        {message}
+        {isStreaming && <span className="animate-pulse">▋</span>}
+      </p>
       {user === CHAT_USER.AI ? <ColorSymbolIcon className="mt-[1.2rem]" /> : null}
     </div>
   );
