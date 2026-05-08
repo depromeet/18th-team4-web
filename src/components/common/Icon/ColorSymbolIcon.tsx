1c6e7b610c719c5089d845fb5b87311a7b2e4c7a
mlnls <tmdals140@naver.com>
2026-05-08 21:20:53 +0900

    feat: 심볼 아이콘 수정

--- src/components/common/Icon/ColorSymbolIcon.tsx
+++ src/components/common/Icon/ColorSymbolIcon.tsx
@@ -1,13 +1,10 @@
 import { type SVGProps } from 'react';
 import { ColorSymbol } from '@/assets';
-import { cn } from '@/lib';
 
 type Props = SVGProps<SVGSVGElement>;
 
 export const ColorSymbolIcon = (props: Props) => {
-  const { className, ...rest } = props;
+  const { ...rest } = props;
 
-  return (
-    <ColorSymbol className={cn('block h-auto w-48 max-w-full shrink-0', className)} {...rest} />
-  );
+  return <ColorSymbol {...rest} />;
 };
