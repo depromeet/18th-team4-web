5d183ec0bb7ef46e6d78a5013252b9f2bcd77fa2
Jihaeee <elinkang02@gmail.com>
2026-05-22 16:28:57 +0900

    fix: 요약하기 완료 후 뒤로가기 시 홈으로 가도록 라우팅 수정

--- src/components/pages/Summary/SummaryHeader.tsx
+++ src/components/pages/Summary/SummaryHeader.tsx
@@ -2,8 +2,9 @@
 
 import { useRouter } from 'next/navigation';
 import { Header, HEADER_VARIANT } from '@/components';
+import { PATH_NAME } from '@/constants';
 
 export const SummaryHeader = () => {
   const router = useRouter();
-  return <Header variant={HEADER_VARIANT.BACK} onBack={() => router.back()} />;
+  return <Header variant={HEADER_VARIANT.BACK} onBack={() => router.push(PATH_NAME.main())} />;
 };
