e584ef1467e9783d2dc60018552aff60c26b60c4
Jihaeee <elinkang02@gmail.com>
2026-05-29 10:04:46 +0900

    feat: 대화 기록 없을 때 화면 구현

--- src/components/pages/Main/EmptyRecordsBody.tsx
+++ src/components/pages/Main/EmptyRecordsBody.tsx
@@ -1,20 +1,22 @@
 import Image from 'next/image';
-import { Shelve } from '@/assets';
+import { emptyIcon } from '@/assets';
 
 export const EmptyRecordsBody = () => {
   return (
-    <main className="flex flex-1 flex-col items-center pt-[8rem] px-[2.4rem]">
-      <div className="relative h-[14.48rem] w-[16rem]">
-        <Image src={Shelve} alt="책장" fill className="object-contain" />
+    <main className="flex flex-1 flex-col items-center justify-center">
+      <div className="relative h-[22.5rem] w-[26.7rem] shrink-0 overflow-hidden">
+        <Image
+          src={emptyIcon}
+          alt="대화 없음"
+          width={301}
+          height={301}
+          className="absolute right-[-1.694rem] top-[-1.694rem] w-[30.088rem]"
+        />
+        <div className="absolute bottom-0 left-0 right-0 h-[11.6rem] bg-linear-to-b from-transparent to-white" />
       </div>
-      <div className="mt-[4rem] flex flex-col items-center gap-[0.2rem]">
-        <h2 className="headline2-extrabold text-center tracking-[-0.06rem] text-text-caption">
-          첫번째 대화를 시작해볼까요?
-        </h2>
-        <p className="body1-medium text-center tracking-[-0.048rem] text-text-disable">
-          아직 저장된 대화가 없어요
-        </p>
-      </div>
+      <p className="title1-bold bg-linear-to-r from-text-default to-green-primary bg-clip-text text-center tracking-[-0.054rem] text-transparent">
+        첫 대화를 시작해볼까요?
+      </p>
     </main>
   );
 };
