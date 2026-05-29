d0e36147b548052244175295e565413fd73d346c
Jihaeee <elinkang02@gmail.com>
2026-05-29 15:27:52 +0900

    style: 불투명도 조절

--- src/components/pages/Main/StartChatFab.tsx
+++ src/components/pages/Main/StartChatFab.tsx
@@ -15,7 +15,7 @@ export const StartChatFab = (props: Props) => {
       <button
         type="button"
         onClick={onClick}
-        className="relative flex cursor-pointer items-center rounded-full bg-white py-[1.2rem] pl-[1.6rem] pr-[1.2rem] shadow-[inset_1.8px_0px_1.8px_0.6px_rgba(0,0,0,0.3),inset_0px_-0.6px_1.2px_0.6px_rgba(0,0,0,0.2),inset_5.5px_5.5px_4px_-4.5px_white,inset_3.6px_5.5px_4.2px_-3.6px_white,inset_-3.6px_-3.6px_1.8px_-3.6px_rgba(255,255,255,0.8)]"
+        className="relative flex cursor-pointer items-center rounded-full bg-white/85 py-[1.2rem] pl-[1.6rem] pr-[1.2rem] shadow-[inset_1.8px_0px_1.8px_0.6px_rgba(0,0,0,0.3),inset_0px_-0.6px_1.2px_0.6px_rgba(0,0,0,0.2),inset_5.5px_5.5px_4px_-4.5px_white,inset_3.6px_5.5px_4.2px_-3.6px_white,inset_-3.6px_-3.6px_1.8px_-3.6px_rgba(255,255,255,0.8)]"
       >
         <span className="body2-bold whitespace-nowrap text-text-default">AI와 대화하기</span>
         <div className="ml-[0.8rem] flex size-[2.4rem] shrink-0 items-center justify-center rounded-full bg-green-darkest shadow-[0px_0px_2.4rem_0px_rgba(0,0,0,0.25)]">
