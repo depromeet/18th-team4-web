344b8043847e1762c1c3e51c243fad0bb36f46ec
Jihaeee <elinkang02@gmail.com>
2026-06-02 12:10:18 +0900

    feat: 온보딩 이미지 assets 추가

--- src/assets/common/webps/index.ts
+++ src/assets/common/webps/index.ts
@@ -2,4 +2,6 @@ export { default as Shelve } from './empty-shelve.webp';
 export * from './gradient';
 export { default as HomeLogo } from './home-logo.webp';
 export { default as Logo } from './logo.webp';
+export { default as Onboarding1 } from './onboarding_1.webp';
+export { default as Onboarding2 } from './onboarding_2.webp';
 export { default as Summary } from './summary.webp';
