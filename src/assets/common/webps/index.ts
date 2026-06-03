4eb69afb8a6de8fa7a2d054110906a5d87219764
poodlepoodle <chammal97@naver.com>
2026-06-03 23:41:01 +0900

    feat: 404 페이지 캐릭터 에셋 추가

--- src/assets/common/webps/index.ts
+++ src/assets/common/webps/index.ts
@@ -1,4 +1,5 @@
 export { default as emptyIcon } from './empty-chat.webp';
+export { default as ErrorCharacter } from './error-404.webp';
 export * from './gradient';
 export { default as HomeLogo } from './home-logo.webp';
 export { default as Logo } from './logo.webp';
