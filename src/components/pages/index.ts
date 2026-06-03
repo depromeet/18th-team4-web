111c1382579e7ee504830a3b6a0cef8d7f591af7
mlnls <tmdals140@naver.com>
2026-06-03 23:33:30 +0900

    feat: 페이지 배럴에 책 상세·마이페이지 목록 컨테이너 export

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,7 +1,9 @@
+export { BookDetailContainer } from './BookDetail/Container';
 export { Chat } from './Chat/Chat';
 export { default as ChatContainer } from './Chat/Container';
 export { MainContainer } from './Main/Container';
 export { MypageContainer } from './Mypage/Container';
+export { MYPAGE_LIST_TAB, MypageListContainer } from './Mypage/ListContainer';
 export { RegisterComplete } from './Register/Complete';
 export { RegisterContainer } from './Register/Container';
 export { SummaryContainer } from './Summary/Container';
