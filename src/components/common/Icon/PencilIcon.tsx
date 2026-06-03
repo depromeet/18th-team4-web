079f911942242d300c47fb0d5ee4947bb8a23274
mlnls <tmdals140@naver.com>
2026-06-03 17:32:13 +0900

    feat: 프로필/연필 아이콘 공통 컴포넌트 추가

---
+++ src/components/common/Icon/PencilIcon.tsx
@@ -0,0 +1,11 @@
+import { type SVGProps } from 'react';
+import { Pencil } from '@/assets';
+import { cn } from '@/lib/utils';
+
+type Props = SVGProps<SVGSVGElement>;
+
+export const PencilIcon = (props: Props) => {
+  const { className, ...rest } = props;
+
+  return <Pencil className={cn('size-8', className)} {...rest} />;
+};
