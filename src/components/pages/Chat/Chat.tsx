5c8a525686ae406713e5b79b99de0f9c7c3d7b50
Saebom <spring180@naver.com>
2026-06-13 01:33:51 +0900

    style: 채팅 시간 스타일 변경

--- src/components/pages/Chat/Chat.tsx
+++ src/components/pages/Chat/Chat.tsx
@@ -31,7 +31,7 @@ export const Chat = (props: Props) => {
   if (isMe) {
     return (
       <div className="flex items-end gap-[0.6rem] self-end">
-        {time && <span className="caption1-medium shrink-0 text-text-caption">{time}</span>}
+        {time && <span className="caption1-medium shrink-0 text-gray-alpha-200">{time}</span>}
         {bubble}
       </div>
     );
