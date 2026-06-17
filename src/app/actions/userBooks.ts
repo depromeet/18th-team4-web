b705cb4592f679fab03f8ff112ee2aa6ee751c33
mlnls <tmdals140@naver.com>
2026-06-17 09:53:30 +0900

    feat: 책 등록 후 revalidate 추가

--- src/app/actions/userBooks.ts
+++ src/app/actions/userBooks.ts
@@ -2,6 +2,11 @@
 
 import { revalidatePath } from 'next/cache';
 
+export const revalidateMypageAction = async () => {
+  revalidatePath('/mypage');
+  revalidatePath('/mypage/list');
+};
+
 export const revalidateHomeAction = async () => {
   revalidatePath('/');
 };
